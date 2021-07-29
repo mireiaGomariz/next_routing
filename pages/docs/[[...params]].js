52fa3875a1a2d9abfd8acfd92a25cc9b1aedc61a
Mireia Perez gomariz <mireiapgomariz@gmail.com>
2021-07-29 09:46:35 +0200

    delete console.log

--- pages/docs/[[...params]].js
+++ pages/docs/[[...params]].js
@@ -4,7 +4,6 @@ import {useRouter} from 'next/router'
 function Doc() {
   const router = useRouter()
   const { params = [] } = router.query
-  console.log(params)
   if (params.length === 2) {
     return (
     <h1>
